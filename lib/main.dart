3aabe70421c9e33fbaee50ce6f194b7b2e4cbf99
sachinv26 <vermas7008@gmail.com>
2023-01-24 21:56:31 +0530

    landing pages added

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_chat/common/routes/pages.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        builder: (BuildContext context,Widget? child)=>GetMaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        ),
+    );
+  }
+}
---
+++ lib/pages/welcome/controller.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_chat/pages/welcome/state.dart';
+import 'package:get/get.dart';
+class WelcomeController extends GetxController{
+  final state = WelcomeState();
+  WelcomeController();
+  changePage(int index) async{
+    state.index.value=index;
+  }
+}
